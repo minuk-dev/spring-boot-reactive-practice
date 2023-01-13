b5ee2f9797d15b6de75e97cfe22d135ad6cb8160
Min Uk Lee <minuk.dev@gmail.com>
2023-01-14 03:06:40 +0900

    hacking-spring-boot-ch1-reactive: something wrong, cannot fetch data

---
+++ hacking-spring-boot-ch1-reactive/src/test/kotlin/com/greglturnquist/hackingspringbootch1reactive/HackingSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootch1reactive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HackingSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
